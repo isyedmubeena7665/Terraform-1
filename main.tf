dd933fee28e467343a807cd64d2d7e5f09411304
isyedmubeena7665 <isyedmubeena17@gmail.com>
2025-04-27 14:14:44 +0530

    second commit

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ provider "aws" {
 
 resource "aws_instance" "ec2" {
     ami = "ami-0e449927258d45bc4"
-    instance_type = "t2.micro"
+    instance_type = var.instance_name
 
     tags = {
         Name = "Machine-1"
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "instance_name" {
+    description = "value"
+    type = "t2.micro"
+  
+}
